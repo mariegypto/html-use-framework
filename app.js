ac4ba8412bee58ba137a16db1def1a3615277a5f
MARIANA EGYPTO FEIGEL <mariana.0769@aluno.pr.senac.br>
2022-09-13 22:28:40 -0300

    Aula dia 13

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 function requestOfServer() {
+
     $.ajax({
-        type:'get',
-        url:"http://localhost:8000/hello-world?info=jheniferbonita"
-    })
-        .done(function(data){
-        console.log(data);
-        document.getElementById('name').innerHTML = data.name
-        document.getElementById('developer').innerHTML = data.manager_developer
+        type: 'get',
+        url: "http://localhost:8000/hello-world?info=OlaMundo"
+    }).done(function(data) { 
+        document.getElementById('developer').innerHTML = data.name
         document.getElementById('version').innerHTML = data.version
-        document.getElementById("value_of_variable").innerHTML = data.value_of_variable_info
-        document.getElementById('company_site').innerHTML = data.web_site_company
+        document.getElementById('value-of-variable').innerHTML = data.value_of_variable_info
+        document.getElementById('company-site').innerHTML = data.web_site_company
 
         $("#show-values").css('display', 'block')
+
     });
+
+}
+
+function submitDataOfUser(){
+    const nameOfUser = $("#name").val()
+    const lastNameOfUser = $("#lastname").val()//hashtag na ffrente é pq é Id
+    const ageOfUser = $("#age").val()
+
+    console.log(nameOfUser)
+    console.log(lastNameOfUser)
+    console.log(ageOfUser)
 }
