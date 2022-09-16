0c192502b324952bd3b9011f3fbcef8df01d1e26
MARIANA EGYPTO FEIGEL <mariana.0769@aluno.pr.senac.br>
2022-09-15 22:25:00 -0300

    Correcoes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function requestOfServer() {
 
     $.ajax({
         type: 'get',
-        url: "http://localhost:8000/hello-world?info=OlaMundo"
+        url: "http://localhost:8000/hello-world?info=Getting"
     }).done(function(data) { 
         document.getElementById('developer').innerHTML = data.name
         document.getElementById('version').innerHTML = data.version
@@ -13,6 +13,20 @@ function requestOfServer() {
 
     });
 
+}
+
+function requestOfServerPost(){
+    $.ajax({
+        type: 'post',
+        url: "http://localhost:8000/hello-rota?info=Posting"
+    }).done(function(data){
+        document.getElementById('developer').innerHTML = data.name
+        document.getElementById('version').innerHTML = data.version
+        document.getElementById('value_of_variable').innerHTML = data.value_of_variable_info
+        document.getElementById('company_site').innerHTML = data.web_site_company
+
+        $("#show-values").css('display','block')
+    });
 }
 
 function submitDataOfUser(){
